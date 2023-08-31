0112e9e6196b7ac667979bb27667150506290a02
Pre-11 <prernakumari7427@gmail.com>
2023-08-31 20:12:24 +0530

    formdata added

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,41 +1,83 @@
 import React from 'react'
 import styles from './CSS/Home.module.css'
-import {  IoMdCloseCircle } from 'react-icons/io';
+import { IoMdCloseCircle } from 'react-icons/io';
+import { useState } from 'react';
+import axios from 'axios'
+ 
+axios.defaults.baseURL = 'http://localhost:8080/'
 
 function Home() {
+
+  const [addSection, setAddSection] = useState(false)
+  const [formdata,setFormData] = useState({
+    name:"",
+    email:'',
+    mobile:"",
+  })
+  const handleChange = (e)=>{
+    const{value,name} = e.target
+    setFormData((prev)=>{
+      return{
+        ...prev,
+        [name]:value
+      }
+    })
+  }
+
+const handleSubmit = async(e)=>{
+  e.preventDefault();
+  const data = await axios.post("/create",formdata)
+  console.log(data)
+  if(data.data.success){
+    setAddSection(false)
+    alert(data.data.message)
+  }
+
+}
+
+
+  
+
   return (
-    <div>
-        <button>Add </button>
-        <div className={styles.container}>
-       <div className={styles.icon}>     
-        <IoMdCloseCircle/>
-        </div> 
-          
+    <div className={styles.box}>
+      <button  className={styles.addBtn} onClick ={()=>setAddSection(true)}>Add </button>
+      {
+        addSection && (
 
-        <form >
-          <label  className={styles.label} hmtlFor="name">Name:</label>
-          <br/>
-            <input className={styles.input} type ="text" label="Name"  />
-            <br/>
-            <br/>
-            
-
-            <label  className={styles.label} >Email:-</label>
-            <br/>
-           
-            <input   className={styles.input}type ="email" id="name" name="name" />
-            <br/>
-            <br/>
-
-            <label   className={styles.label} >Mobile Number:-</label>
-            <br/>
-            <input   className={styles.input} type = "number"  />
-            <br/>
-            <input  className={styles.button} type ="submit" />
-        </form>
-
+          <div className={styles.container}>
+          <div className={styles.icon} onClick ={()=>setAddSection(false)}>
+            <IoMdCloseCircle />
+          </div>
+  
+  
+          <form  onSubmit={handleSubmit}>
+            <label className={styles.label} hmtlFor="name">Name:</label>
+            <br />
+            <input className={styles.input} type="text" label="name"  name="name" onChange={handleChange}/>
+            <br />
+            <br />
+  
+  
+            <label className={styles.label} >Email:-</label>
+            <br />
+  
+            <input className={styles.input} type="email" id="name" name="email" onChange={handleChange} />
+            <br />
+            <br />
+  
+            <label className={styles.label} >Mobile Number:-</label>
+            <br />
+            <input className={styles.input} type="number" name="mobile" onChange={handleChange} />
+            <br />
+            <input className={styles.button} type="submit" />
+          </form>
+  
         </div>
-        
+
+        )
+      }
+     
+
     </div>
   )
 }
